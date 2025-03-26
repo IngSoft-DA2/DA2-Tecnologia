2f22aff2068ee988d76465ed14ca8e82519e403e
Federico González Buga <112187044+fedegb5@users.noreply.github.com>
2025-03-26 12:45:59 -0300

    [GREEN] Create_WhenCorrectInfo_ShouldCreateMovie (#7)
    Co-authored-by: Daniel Acevedo <daniel18acevedo@gmail.com>

--- 2- Moq Service/Vidly.BusinessLogic.Test/MovieServiceTest.cs
+++ 2- Moq Service/Vidly.BusinessLogic.Test/MovieServiceTest.cs
@@ -9,13 +9,13 @@ namespace Vidly.BusinessLogic.Test
     public sealed class MovieServiceTest
     {
         private Mock<IMovieRepository> _movieRepositoryMock;
-        private MovieService _service;
+        private MovieService _movieService;
 
         [TestInitialize]
         public void Initialize()
         {
             _movieRepositoryMock = new Mock<IMovieRepository>(MockBehavior.Strict);
-            _service = new MovieService(_movieRepositoryMock.Object);
+            _movieService = new MovieService(_movieRepositoryMock.Object);
         }
 
         #region Create
@@ -29,17 +29,48 @@ namespace Vidly.BusinessLogic.Test
                 DateTimeOffset.UtcNow.AddMonths(-1));
 
             _movieRepositoryMock
-                .Setup(i => i.Exists(i => i.Title == args.Title))
+                .Setup(mock => mock.Exists(movie => movie.Title == args.Title))
                 .Returns(true);
 
-            var act = () => _service.Add(args);
+            var act = () => _movieService.Add(args);
 
             act.Should().Throw<Exception>().WithMessage("Movie duplicated");
+
+            // Esta por gusto abajo, porque si estuviese arriba nunca se llamaria _movieService.Add(args)
             _movieRepositoryMock.VerifyAll();
         }
         #endregion
 
         #region Success
+
+        [TestMethod]
+        public void Create_WhenCorrectInfo_ShouldCreateMovie()
+        {
+            var args = new CreateMovieArgs(
+                "some name",
+                "some description",
+                DateTimeOffset.UtcNow);
+
+
+            _movieRepositoryMock
+                .Setup(mock => mock.Exists(It.IsAny<Expression<Func<Movie, bool>>>()))
+                .Returns(false);
+
+            _movieRepositoryMock.
+                Setup(mock => mock.Create(It.Is<Movie>( m =>
+                m.Title == args.Title &&
+                m.Description == args.Description &&
+                m.PublishedOn == args.PublishedOn)));
+
+            var response = _movieService.Add(args);
+
+            // Esta por gusto primero, porque no se tiene la variable act como en la prueba anterior
+            _movieRepositoryMock.VerifyAll();
+            
+            response.Should().NotBeNull();
+            response.Id.Should().NotBeNull();
+            response.Id.Should().NotBeEmpty();
+        }
         #endregion
         #endregion
     }
